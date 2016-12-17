c4d6179c7dbfdfaaf2bde084d9b199fe5087a58f
ZnamenskayaVictoria <znamenskayavica@gmail.com>
2016-12-17 19:57:43 +0300

    Api controllers

---
+++ ServerLogicSource/TdataBase/Controllers/PerfectDateController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using TdataBase.Models;
+
+namespace TdataBase.Controllers
+{
+    public class PerfectDateController : ApiController
+    {
+        private PartnersContext db = new PartnersContext();
+
+        // api/PerfectDate/month/day
+        //Returns perfect date for given one
+        public PerfectDate GetDate(int month, int day)
+        {
+            try
+            {
+                DateTime target = new DateTime(1996, month, day);
+                return new PerfectDate(db.Partner.ToList(), target);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ServerLogicSource/TdataBase/Controllers/ZodiacSignsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using TdataBase.Models;
+
+namespace TdataBase.Controllers
+{
+    public class ZodiacSignsController : ApiController
+    {
+        private PartnersContext db = new PartnersContext();
+        // api/ZodiacSigns/id
+        //Returns statistic for given zodiac sign
+        public ZodiacSigns GetSigns(int id)
+        {
+            if (id < 0 || id > 11 )
+                return null;
+            List<Partners> partners = (from par in db.Partner
+                                       where par.Zodiac == id
+                                       select par).ToList();
+            ZodiacSigns signs = new ZodiacSigns(partners);
+            return signs;
+        }
+    }
+}
