6106b8f3e3aee214d0e2e528bfc1bea50b0ebf94
ZnamenskayaVictoria <znamenskayavica@gmail.com>
2016-12-17 19:54:35 +0300

    Two different return types
    ZodiacSigns contains statistic for given zodiac sign
    PerfectDate calculate perfect date of birth for given date

---
+++ ServerLogicSource/TdataBase/Models/PerfectDate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TdataBase.Models
+{
+    public class PerfectDate
+    {
+        /*
+         * Create new object with date, perfect for given first partners date
+         */
+        public PerfectDate(List<Partners> partners, DateTime target)
+        {
+            Date = target;
+            DateTime start = new DateTime(1996, 1, 1);
+            partners.ForEach(x =>
+            {
+                try
+                {
+                    
+                    DateTime first = new DateTime(1996, x.Month, x.Day);
+                    DateTime second = new DateTime(1996, x.SecMonth, x.SecDay);
+
+                    //Difference between sample and target
+                    int diff = (first - target).Days % 182;
+
+                    //Differences coefficient 
+                    double coef = diff > 0 ? 1 - diff / 182.5 : -1 - diff / 185.2;
+                    if (coef >= MaxCoef)
+                    {
+                        Exists = second;
+                        MaxCoef = coef;
+                    }
+
+                    int targetdays = (Date - start).Days;
+                    int seconddays = (second - start).Days;
+
+                    //Counting new date
+                    int daysDiff = (int)(targetdays + seconddays * coef) / 2;
+                    Date = start.AddDays(daysDiff);
+
+                }
+                catch
+                {
+                    Console.WriteLine($"Incorrect data: {x.Month} {x.Day}, {x.SecMonth} {x.SecDay}");
+                }
+            });
+
+        }
+        //Perfect date
+        public DateTime Date { get; set; }
+
+        //Maximum coefficient
+        public double MaxCoef { get; set; } = 0;
+
+        //Sample date with maximum coefficient
+        public DateTime Exists { get; set; }
+    }
+}
---
+++ ServerLogicSource/TdataBase/Models/ZodiacSigns.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+namespace TdataBase.Models
+{
+    public class ZodiacSigns
+    {
+        //Counting statistic of different signs for given one
+        public ZodiacSigns(List<Partners> partners)
+        {
+            Total = partners.Count;
+            partners.ForEach(x =>
+            {
+                switch (x.SecZodiac)
+                {
+                    case 0:
+                        Aries++;
+                        break;
+                    case 1:
+                        Taurus++;
+                        break;
+                    case 2:
+                        Gemini++;
+                        break;
+                    case 3:
+                        Cancer++;
+                        break;
+                    case 4:
+                        Leo++;
+                        break;
+                    case 5:
+                        Virgo++;
+                        break;
+                    case 6:
+                        Libra++;
+                        break;
+                    case 7:
+                        Scorpio++;
+                        break;
+                    case 8:
+                        Sagittarius++;
+                        break;
+                    case 9:
+                        Capricorn++;
+                        break;
+                    case 10:
+                        Aquarius++;
+                        break;
+                    case 11:
+                        Pisces++;
+                        break;
+                }
+            });
+        }
+        public int Total { get; set; }
+        public int Aries { get; set; }
+        public int Taurus { get; set; }
+        public int Gemini { get; set; }
+        public int Cancer { get; set; }
+
+
+        public int Leo { get; set; }
+        public int Virgo { get; set; }
+        public int Libra { get; set; }
+        public int Scorpio { get; set; }
+
+
+        public int Sagittarius { get; set; }
+        public int Capricorn { get; set; }
+        public int Aquarius { get; set; }
+        public int Pisces { get; set; }
+
+    }
+}
